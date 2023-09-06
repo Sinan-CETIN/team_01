ea02cda678d89a29cd24c7859dc6429edaa04c4a
Sinan <sinanctn54@gmail.com>
2023-09-06 12:11:49 -0500

    Added Question_05_12

---
+++ src/week_05/assignments/Question_05_12.java
@@ -0,0 +1,11 @@
+package week_05.assignments;
+
+public class Question_05_12 {
+    public static void main(String[] args) {
+        int number = 1;
+        while(Math.pow(number,2) < 12000) {
+            number++;
+        }
+        System.out.println("The smallest n is -> " + number);
+    }
+}
