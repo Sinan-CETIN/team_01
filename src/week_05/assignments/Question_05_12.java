b28216514784a8cfebfc42f97a973892ba837377
Sinan <sinanctn54@gmail.com>
2023-09-06 12:21:42 -0500

    formatted Question_05_12

--- src/week_05/assignments/Question_05_12.java
+++ src/week_05/assignments/Question_05_12.java
@@ -3,11 +3,11 @@ package week_05.assignments;
 import java.util.Scanner;
 
 public class Question_05_12 {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         int number = 1;
-        while(Math.pow(number,2) < 12000) {
+        while (Math.pow(number, 2) < 12000) {
             number++;
         }
         System.out.println("The smallest n is -> " + number);
-
+    }
 }
